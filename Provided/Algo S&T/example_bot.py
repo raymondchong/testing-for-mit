247360fcc80869af839489b44ae230a6157860af
Lefbomb <jwlefbom@gmail.com>
2018-11-04 13:52:34 -0500

    added algo st output
    check out the message feedback

--- Provided/Algo S&T/example_bot.py
+++ Provided/Algo S&T/example_bot.py
@@ -2,8 +2,9 @@ from tradersbot import *
 from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
-import py_vollib
+# import py_vollib
 
+t = TradersBot('127.0.0.1', 'trader0', 'trader0')
 #Initialize variables: positions, expectations, future customer orders, etc
 position_limit = 5000
 case_length = 450
@@ -14,53 +15,36 @@ time = 0
 topBid = 0
 topAsk = 0
 #etc etc
-"""
-py_vollib.black_scholes_merton.implied_volatility.implied_volatility(price, S, K, t, r, q, flag)[source]
-Calculate the Black-Scholes-Merton implied volatility.
 
-Parameters:	
-S (float) – underlying asset price
-K (float) – strike price
-sigma (float) – annualized standard deviation, or volatility
-t (float) – time to expiration in years
-r (float) – risk-free interest rate
-q (float) – annualized continuous dividend rate
-flag (str) – ‘c’ or ‘p’ for call or put.
->>> S = 100
->>> K = 100
->>> sigma = .2
->>> r = .01
->>> flag = 'c'
->>> t = .5
->>> q = 0
->>> price = black_scholes_merton(flag, S, K, t, r, sigma, q)
->>> iv = implied_volatility(price, S, K, t, r, q, flag)
->>> expected_price = 5.87602423383
->>> expected_iv = 0.2
-"""
 
 
 def register(msg, TradersOrder):
+    print(msg)
     #Set case information
     pass
 
-def update_market(TradersOrder):
+def update_market(msg, TradersOrder):
+    print(msg)
     #Update market information
     pass
 
-def update_trader(TradersOrder):
+def update_trader(msg, TradersOrder):
+    print(msg)
     #Update positions
     pass
 
 def update_trade(msg, TradersOrder):
+    print(msg)
     #Update trade information
     pass
 
 def update_order(msg, TradersOrder):
+    print(msg)
     #Update order information
     pass
 
 def update_news(msg, TradersOrder):
+    print(msg)
     #Update news information
     pass
 
@@ -68,7 +52,6 @@ def process():
     #Do stuff to trade
     pass
 
-t = TradersBot('127.0.0.1', 'trader0', 'trader0')
 
 t.onAckRegister = register
 t.onMarketUpdate = update_market
